23fabea5e68abdec558e1f26500262c4a7bb93ac
Raul <jimenezraul1981@gmail.com>
2023-06-28 19:22:58 -0400

    deploy

--- src/main/java/com/raul/blogapi/utils/Cookies.java
+++ src/main/java/com/raul/blogapi/utils/Cookies.java
@@ -20,7 +20,6 @@ public class Cookies {
     private static void setCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setMaxAge(maxAge);
-        cookie.setPath("/");
         cookie.setSecure(true);
         cookie.setHttpOnly(true);
         cookie.setAttribute("SameSite", "none");
