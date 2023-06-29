0dadbe0721c93e82225977e2220f9ff8a752b77a
Raul <jimenezraul1981@gmail.com>
2023-06-28 23:50:14 -0400

    deploy

--- src/main/java/com/raul/blogapi/utils/Cookies.java
+++ src/main/java/com/raul/blogapi/utils/Cookies.java
@@ -24,7 +24,7 @@ public class Cookies {
         cookie.setMaxAge(maxAge);
         cookie.setSecure(true);
         cookie.setHttpOnly(true);
-        cookie.setAttribute("SameSite", "Strict");
+        cookie.setAttribute("SameSite", "Lax");
         cookie.setDomain("illustrious-sprite-a61321.netlify.app");
         response.addCookie(cookie);
     }
