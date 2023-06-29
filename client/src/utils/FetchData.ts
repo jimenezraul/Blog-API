9bb7e7b00671b9486cfd99262864fef7df9b8547
Raul <jimenezraul1981@gmail.com>
2023-06-28 20:45:27 -0400

    deploy

--- client/src/utils/FetchData.ts
+++ client/src/utils/FetchData.ts
@@ -2,7 +2,7 @@ import { store } from '../app/store';
 import Auth from '../auth';
 import { setAlert } from '../app/features/alertSlice';
 
-const BASE_URL = 'https://mysterious-headland-44233-fdefe22bd12e.herokuapp.com';
+const BASE_URL = 'http://localhost:8080';
 
 export const FetchData: any = async (
   endpoint: string,
